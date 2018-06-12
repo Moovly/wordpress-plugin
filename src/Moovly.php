c4660f4b7991a92877547c22171d4f9c5523033d
Laurens Bultynck <laurens@MacBook-Pro-van-Laurens.local>
2018-06-12 17:43:12 +0200

    Refactor main actions to initialize method

--- src/Moovly.php
+++ src/Moovly.php
@@ -17,35 +17,31 @@ class Moovly
 
     public function initialize()
     {
-        $this->version = get_plugin_data(__DIR__ . '/../moovly.php')['Version'];
-        $this->registerAssets();
-        $this->addMenuItems();
         $this->settings->initialize();
+        $this->version = get_plugin_data(__DIR__ . '/../moovly.php')['Version'];
+        add_action('admin_enqueue_scripts', [$this, 'registerAssets']);
+        add_action('admin_menu', [$this, 'addMenuItems']);
     }
 
-    private function registerAssets()
+    public function registerAssets($page)
     {
-        add_action('admin_enqueue_scripts', function ($page) {
-            if (strpos($page, 'moovly') !== false) {
-                wp_enqueue_style('moovly', plugins_url("moovly/dist/moovly.css"), $dependencies = [], $this->version, $media = 'all');
-                wp_register_script('moovly', plugins_url("moovly/dist/moovly.js"), $dependencies = [], $this->version, $in_footer = true);
-                wp_enqueue_script('moovly');
+        if (strpos($page, 'moovly') !== false) {
+            wp_enqueue_style('moovly', plugins_url("moovly/dist/moovly.css"), $dependencies = [], $this->version, $media = 'all');
+            wp_register_script('moovly', plugins_url("moovly/dist/moovly.js"), $dependencies = [], $this->version, $in_footer = true);
+            wp_enqueue_script('moovly');
+        }
+    }
+
+    public function addMenuItems()
+    {
+        add_menu_page(
+            __('Moovly', 'moovly'),
+            __('Moovly', 'moovly'),
+            'manage_options',
+            'moovly',
+            function () {
+                return $this->settings->makeView();
             }
-        });
-    }
-
-    private function addMenuItems()
-    {
-        add_action('admin_menu', function () {
-            add_menu_page(
-                __('Moovly', 'moovly'),
-                __('Moovly', 'moovly'),
-                'manage_options',
-                'moovly',
-                function () {
-                    return $this->settings->makeView();
-                }
-            );
-        });
+        );
     }
 }
