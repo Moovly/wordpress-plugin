755834f8a23333fbe2ec4ee9e46692cdf0622b41
Laurens Bultynck <laurens@MacBook-Pro-van-Laurens.local>
2018-06-13 17:40:41 +0200

    Move assets folder to root level

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
 let mix = require('laravel-mix');
 
 mix
-    .sass('src/assets/sass/app.scss', 'moovly.css')
-    .js('src/assets/js/app.js', 'moovly.js')
+    .sass('assets/sass/app.scss', 'moovly.css')
+    .js('assets/js/app.js', 'moovly.js')
     .setPublicPath('dist');
