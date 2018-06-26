d0e2fc548167ba265c10dcd1bf9effb1ff50412b
Laurens Bultynck <laurens@MacBook-Pro-van-Laurens.local>
2018-06-26 14:43:35 +0200

    Update moovly templates component with post template

--- assets/js/back/components/MoovlyTemplates.vue
+++ assets/js/back/components/MoovlyTemplates.vue
@@ -11,7 +11,7 @@
                                 <th scope="col">Thumbnail</th>
                                 <th scope="col">Template name</th>
                                 <th scope="col">Shortcode</th>
-                                <th scope="col">Preview</th>
+                                <th scope="col" class="text-center">Use for posts</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -20,7 +20,16 @@
                                 <th><img :src="template.thumbnail" style="max-width: 75px"/></th>
                                 <th>{{ template.title }}</th>
                                 <th><pre><code>{{ template.shortcode }}</code></pre></th>
-                                <th><a :href="template.preview">See preview</a></th>
+                                <th class="text-center">
+                                    <input
+                                        type="radio"
+                                        :id="template.id"
+                                        :value="template.identifier"
+                                        v-model="ui.selectedTemplate"
+                                        :checked="isChecked(template)"
+                                        @change="updatePostTemplates"
+                                    >
+                                    </th>
                             </tr>
                         </tbody>
                     </table>
@@ -38,27 +47,60 @@
         },
 
         mounted() {
-            this.fetch();
+            Promise.all([
+                this.fetch(),
+                this.getPostTemplates(),
+            ]);
         },
 
         data() {
             return {
                 ui: {
                     templates: [],
+                    postTemplates: [],
+                    selectedTemplate: null,
                     loading: false,
                 },
                 templates: {
                     index: `${window.location.origin}/wp-json/moovly/v1/templates/index`,
-                }
+                    settings: `${window.location.origin}/wp-json/moovly/v1/templates/settings`,
+                },
             }
         },
 
         methods: {
             fetch() {
                 this.ui.loading = true;
-                axios.get(this.templates.index).then(response => {
+                return axios.get(this.templates.index)
+                .then(response => {
                     this.ui.templates = response.data;
                     this.ui.loading = false;
+                });
+            },
+
+            getPostTemplates() {
+                return axios.get(this.templates.settings)
+                .then(response => {
+                    this.ui.postTemplates = response.data.post_templates;
+                    this.ui.selectedTemplate = this.ui.postTemplates[0].id;
+                });
+            },
+
+            updatePostTemplates() {
+                axios.post(this.templates.settings, {
+                    post_templates: [
+                        this.ui.selectedTemplate,
+                    ],
+                }).then(response => {
+                    this.ui.selectedTemplate = response.data.post_templates[0].id;
+                }).catch(error => {
+                    console.log(error);
+                })
+            },
+
+            isChecked(template) {
+                return !! this.ui.postTemplates.find(postTemplate => {
+                    return postTemplate.id === template.identifier;
                 });
             }
         }
