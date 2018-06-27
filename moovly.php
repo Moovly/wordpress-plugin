b21636dca3b853c46d8476bb670b1d4874720c7e
Laurens Bultynck <laurens@MacBook-Pro-van-Laurens.local>
2018-06-27 17:13:22 +0200

    Update plugin info for first release

--- moovly.php
+++ moovly.php
@@ -3,9 +3,10 @@
  * Plugin Name: Moovly
  * Description: An integration plugin for the Moovly API.
  * Author: Moovly
- * Author URI:
+ * Author URI: https://www.moovly.com
  * Version: 1.0.0
- * Plugin URI:
+ * Plugin URI: https://github.com/Moovly/wordpress-plugin
+
  */
 
 require __DIR__ . '/vendor/autoload.php';
