67054305072d01832967e1c8e868a504abe2f7a3
Niels <niels.vermaut@moovly.com>
2018-09-27 18:23:38 +0200

    Update MoovlyFileVariable.vue

--- assets/js/front/components/variables/MoovlyFileVariable.vue
+++ assets/js/front/components/variables/MoovlyFileVariable.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="form-group">
-    <label :for="variable.id"> {{ variable.label}} <small>({{variable.name}})</small></label>
+    <label :for="variable.id"> {{ variable.label}}</label>
     <template v-if="!ui.object">
       <input
           type="file"
