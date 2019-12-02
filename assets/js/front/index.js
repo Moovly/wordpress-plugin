59f921c9773393a8b2bcd5c91aa8154fdb099817
Michiel Van Haute <michiel@michvh.be>
2019-12-02 10:43:39 +0100

    update plugin logic

--- assets/js/front/index.js
+++ assets/js/front/index.js
@@ -1,11 +1,16 @@
-import { MoovlyPlugins } from "@moovly/plugins-embed";
+import { MoovlyPlugin } from "@moovly/plugins-embed";
 
 const getElementAndRenderCorrectComponent = (className, Component) => {
   const elements = document.getElementsByClassName(className);
-
   for (let i = 0; i < elements.length; i++) {
     const element = elements[i];
-    debugger;
+    MoovlyPlugin.setProxies({
+      "fetch-template": "/wp-json/moovly/v1/templates/:id"
+    });
+    MoovlyPlugin.Templates.QuickEdit({
+      containerId: element.id,
+      templateId: element.dataset.id
+    });
   }
 };
 
