d4384aa590be686c0ad3cda0821aff9a2fe9ba80
Michiel Van Haute <michiel@michvh.be>
2019-12-05 14:59:20 +0100

    update plugin

--- assets/js/front/index.js
+++ assets/js/front/index.js
@@ -2,19 +2,19 @@ import "./common/bootstrap";
 import "./vue";
 
 import { MoovlyPlugin } from "@moovly/plugins-embed";
-
+MoovlyPlugin.setProxies({
+  "fetch-template": "/wp-json/moovly/v1/templates/:id",
+  "upload-object": "/wp-json/moovly/v1/objects/upload",
+  "template-job-create": "/wp-json/moovly/v1/templates/:id/store",
+  "template-job-poll": "/wp-json/moovly/v1/jobs/:id/status"
+});
 const getElementAndRenderCorrectComponent = (className, Component) => {
   const elements = document.getElementsByClassName(className);
   for (let i = 0; i < elements.length; i++) {
     const element = elements[i];
-    MoovlyPlugin.setProxies({
-      "fetch-template": "/wp-json/moovly/v1/templates/:id",
-      "upload-object": "/wp-json/moovly/v1/objects/upload",
-      "template-job-create": "/wp-json/moovly/v1/templates/:id/store",
-      "template-job-poll": "/wp-json/moovly/v1/jobs/:id/status"
-    });
-    MoovlyPlugin.Templates.QuickEdit({
-      containerId: element.id,
+    element.classList.remove("moovly-template");
+    new MoovlyPlugin.Templates.QuickEdit({
+      container: element,
       templateId: element.dataset.id,
       withPreview: true,
       pollTillSuccess: true
