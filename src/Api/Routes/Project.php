6bb5f77c94202902642d9913a8fd8244f4b0bd62
Michiel Van Haute <michiel@michvh.be>
2021-11-24 10:26:08 +0100

    update wp

--- src/Api/Routes/Project.php
+++ src/Api/Routes/Project.php
@@ -51,6 +51,10 @@ class Project extends Api
             'methods' => 'GET',
             'callback' => [$this, 'projectRenders'],
         ]);
+        register_rest_route($this->namespace, '/(?P<id>[^/]+)/renders/(?P<render_id>[^/]+)', [
+            'methods' => 'DELETE',
+            'callback' => [$this, 'deleteProjectRender'],
+        ]);
     }
 
     /**
@@ -63,6 +67,8 @@ class Project extends Api
         if (!$this->index_permissions()) {
             $this->checkShortcodePermission(ProjectsShortCodeFactory::$tag);
         }
+
+
         $page = $request->get_param('page') ? intval($request->get_param('page')) : 1;
         $pageSize = $request->get_param('page_size') ? intval($request->get_param('page_size')) : 25;
 
@@ -120,6 +126,24 @@ class Project extends Api
         return array_map(function ($render) {
             return Render::transform($render);
         }, $project->getRenders());
+    }
+
+    /**
+     * @param \WP_REST_Request $request
+     *
+     * @return array|\WP_Error
+     */
+    public function deleteProjectRender($request)
+    {
+
+        $this->checkShortcodePermission(RendersShortCodeFactory::$tag);
+        try {
+            $this->getMoovlyService()->deleteRender($request->get_param('render_id'));
+        } catch (\Exception $e) {
+            return $this->throwWPError(null, $e);
+        }
+
+        return ["success" => true];
     }
 
 
