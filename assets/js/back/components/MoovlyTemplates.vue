5e04fac29a03888ba07392e6725e1640712fd8fb
michielvh <michiel@michvh.be>
2022-11-18 11:40:58 +0100

    add template setting

--- assets/js/back/components/MoovlyTemplates.vue
+++ assets/js/back/components/MoovlyTemplates.vue
@@ -11,24 +11,86 @@
 
             <h4>Loading your Templates</h4>
             <small>
-              ProTip: If you want the form submitters to receive a copy of the video, enable "Send result to form
-              submittors" in the Moovly Dashboard when creating a template.
+              ProTip: If you want the form submitters to receive a copy of the
+              video, enable "Send result to form submittors" in the Moovly
+              Dashboard when creating a template.
             </small>
           </div>
-
-          <table class="table table-moovly" id="table-templates" v-if="!ui.loading">
+          <div v-if="!ui.loading" class="plugin-header">
+            <div>
+              <h5 style="margin-bottom: 10px;">Shortcode settings</h5>
+              <div class="form-check">
+                <input
+                  type="checkbox"
+                  name="publishToYoutube"
+                  id="publishToYoutube"
+                  class="form-check-input"
+                  v-model="shortcodeSettings.publishToYoutube"
+                />
+                <label for="publishToYoutube" class="form-check-label">
+                  Publish video to youtube
+                </label>
+              </div>
+              <div class="form-check">
+                <input
+                  type="checkbox"
+                  name="createProject"
+                  id="createProject"
+                  class="form-check-input"
+                  v-model="shortcodeSettings.createProject"
+                />
+                <label for="createProject" class="form-check-label">
+                  Create project from template submission
+                </label>
+              </div>
+              <div class="form-check">
+                <input
+                  type="checkbox"
+                  name="createRender"
+                  id="createRender"
+                  class="form-check-input"
+                  v-model="shortcodeSettings.createRender"
+                />
+                <label for="createRender" class="form-check-label">
+                  Create render from template
+                </label>
+              </div>
+              <div class="form-check">
+                <input
+                  type="checkbox"
+                  name="pollTillSuccess"
+                  id="pollTillSuccess"
+                  class="form-check-input"
+                  v-model="shortcodeSettings.pollTillSuccess"
+                />
+                <label for="pollTillSuccess" class="form-check-label">
+                  After submission, poll till video is done and show video
+                </label>
+              </div>
+            </div>
+          </div>
+          <table
+            class="table table-moovly"
+            id="table-templates"
+            v-if="!ui.loading"
+          >
             <thead>
               <tr>
                 <th scope="col">#</th>
                 <th scope="col">Thumbnail</th>
                 <th scope="col">Template name</th>
                 <th scope="col">Shortcode</th>
-                <th scope="col" class="text-center">Sends result to form submitter</th>
+                <th scope="col" class="text-center">
+                  Sends result to form submitter
+                </th>
                 <th scope="col" class="text-center">Use for posts</th>
               </tr>
             </thead>
             <tbody>
-              <tr v-for="(template, index) in ui.templates" :key="template.identifier">
+              <tr
+                v-for="(template, index) in ui.templates"
+                :key="template.identifier"
+              >
                 <td scope="row">{{ index + 1 }}</td>
                 <td>
                   <img :src="template.thumbnail" style="max-width: 75px" />
@@ -37,7 +99,9 @@
                 <td>
                   <moovly-shortcode :shortcode="template.shortcode" />
                 </td>
-                <td class="text-center">{{ template.is_email_enabled ? 'Yes' : 'No'}}</td>
+                <td class="text-center">
+                  {{ template.is_email_enabled ? "Yes" : "No" }}
+                </td>
                 <td class="text-center">
                   <input
                     type="radio"
@@ -64,12 +128,12 @@ import MoovlyShortcode from "./shared/MoovlyShortcode";
 export default {
   props: {
     restApiCall: {
-      required: true
-    }
+      required: true,
+    },
   },
   components: {
     MoovlyHeader,
-    MoovlyShortcode
+    MoovlyShortcode,
   },
 
   mounted() {
@@ -82,26 +146,53 @@ export default {
         templates: [],
         postTemplates: [],
         selectedTemplate: null,
-        loading: false
+        loading: false,
+      },
+      shortcodeSettings: {
+        publishToYoutube: false,
+        createProject: false,
+        createRender: true,
+        pollTillSuccess: true,
       },
       templates: {
         index: `${this.restApiCall}moovly/v1/templates/index`,
-        settings: `${this.restApiCall}moovly/v1/templates/settings`
-      }
+        settings: `${this.restApiCall}moovly/v1/templates/settings`,
+      },
     };
   },
-
+  watch: {
+    shortcodeSettings: {
+      handler(val) {
+        this.setTemplateShortCode();
+      },
+      deep: true,
+    },
+  },
   methods: {
+    setTemplateShortCode() {
+      this.ui.templates = this.ui.templates.map((temp) => ({
+        ...temp,
+        shortcode: `[moovly-template id='${temp.id}' publish-to-youtube='${
+          this.shortcodeSettings.publishToYoutube ? "1" : 0
+        }' create-project='${
+          this.shortcodeSettings.createProject ? "1" : 0
+        }' create-render='${
+          this.shortcodeSettings.createRender ? "1" : 0
+        }' poll-till-success='${
+          this.shortcodeSettings.pollTillSuccess ? "1" : 0
+        }' ]`,
+      }));
+    },
     fetch() {
       this.ui.loading = true;
-      return axios.get(this.templates.index).then(response => {
+      return axios.get(this.templates.index).then((response) => {
         this.ui.templates = response.data;
         this.ui.loading = false;
       });
     },
 
     getPostTemplates() {
-      return axios.get(this.templates.settings).then(response => {
+      return axios.get(this.templates.settings).then((response) => {
         this.ui.postTemplates = response.data.post_templates;
         this.ui.selectedTemplate = this.ui.postTemplates[0].id;
       });
@@ -110,21 +201,21 @@ export default {
     updatePostTemplates() {
       axios
         .post(this.templates.settings, {
-          post_templates: [this.ui.selectedTemplate]
+          post_templates: [this.ui.selectedTemplate],
         })
-        .then(response => {
+        .then((response) => {
           this.ui.selectedTemplate = response.data.post_templates[0].id;
         })
-        .catch(error => {
+        .catch((error) => {
           console.log(error);
         });
     },
 
     isChecked(template) {
-      return !!this.ui.postTemplates.find(postTemplate => {
+      return !!this.ui.postTemplates.find((postTemplate) => {
         return postTemplate.id === template.identifier;
       });
-    }
-  }
+    },
+  },
 };
 </script>
