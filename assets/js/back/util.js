f5ce0ffc5ebc0d0650dfd1092d061e940717d65c
michielvh <michiel@michvh.be>
2023-02-23 11:45:45 +0100

    update

--- assets/js/back/util.js
+++ assets/js/back/util.js
@@ -1,16 +1,17 @@
 import Toastify from "toastify-js";
 
-const successToast = (text, background) =>
+const successToast = (text, background, type) =>
   Toastify({
     text,
     duration: 2000,
     close: true,
     gravity: "bottom",
+    className: `toast-${type}`,
     position: "center",
     style: { background },
   }).showToast();
 
 export default {
-  toastSuccess: (text) => successToast(text, "#3c8f49"),
-  toastError: (text) => successToast(text, "#d42242"),
+  toastSuccess: (text) => successToast(text, "#3c8f49", "success"),
+  toastError: (text) => successToast(text, "#d42242", "error"),
 };
