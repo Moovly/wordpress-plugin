a74aa7d346f1ff7310751e6743cb46c5f1d5afae
michielvh <michiel@michvh.be>
2023-05-15 16:36:23 +0200

    update packages + plugins

--- assets/js/back/app.js
+++ assets/js/back/app.js
@@ -1,23 +1,21 @@
 import "toastify-js/src/toastify.js";
 require("./bootstrap");
-import Vue from "vue";
+import { createApp } from "vue";
 import Moovly from "./components/Moovly";
 import MoovlySettings from "./components/MoovlySettings";
 import MoovlyTemplates from "./components/MoovlyTemplates";
 import MoovlyProjects from "./components/MoovlyProjects";
 import MoovlyPostVideos from "./components/MoovlyPostVideos";
 
-const moovly = {
-  settings: new Vue({
-    el: "#wpbody-content",
-    components: {
-      Moovly,
-      MoovlySettings,
-      MoovlyTemplates,
-      MoovlyProjects,
-      MoovlyPostVideos,
-    },
-  }),
+const RootComponent = {
+  components: {
+    Moovly,
+    MoovlySettings,
+    MoovlyTemplates,
+    MoovlyProjects,
+    MoovlyPostVideos,
+  },
 };
 
-window.moovly = moovly;
+const app = createApp(RootComponent);
+app.mount("#wpbody-content");
